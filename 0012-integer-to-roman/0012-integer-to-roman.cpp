80f32c26ae627640c6eae11b8c285e78dc2abe79
Ravneet Singh <ravneetsingh290@gmail.com>
2025-01-25 10:30:39 +0530

    Time: 1 ms (73.83%), Space: 9 MB (88.99%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -5,7 +5,7 @@ public:
         string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
         string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
         string ths[]={"","M","MM","MMM"};
-        
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];  
     }
 };
