ce0c16847b587cc306557f4e6d4f719bfc2663cf
Ravneet Singh <ravneetsingh290@gmail.com>
2025-01-25 10:38:28 +0530

    Time: 7 ms (86.67%), Space: 26.6 MB (99.38%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int minimumLength(string s) {
-        vector<int> charFrequency(26, 0);
-        int totalLength = 0;
-        for (char currentChar : s) {
-            charFrequency[currentChar - 'a']++;
+    int minimumLength(const string &s) {
+        std::array<int, 26> cnt_arr;
+        for (char ch : s) {
+            int idx = ch - 'a';
+            cnt_arr[idx]++;
+            /*
+            if (cnt_arr[idx] >= 3)
+                cnt_arr[idx] -= 2;
+                */
         }
-        for (int frequency : charFrequency) {
-            if (frequency == 0) continue;
-            if (frequency % 2 == 0) {
-                totalLength += 2;
-            } else {
-                totalLength += 1;
+        int res = 0;
+        for (int val : cnt_arr) {
+            if (val) {
+                if (val % 2 == 1)
+                    res += 1;
+                else
+                    res += 2;
             }
         }
-        return totalLength;
+        return res;
     }
 };
