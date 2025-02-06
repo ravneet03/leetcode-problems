f2f4c12ab9f0e3d2d92758d408602beb0e497dd0
Ravneet Singh <ravneetsingh290@gmail.com>
2025-02-06 13:09:25 +0530

    Time: 133 ms (98.12%), Space: 27.7 MB (96.87%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -1,18 +1,32 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        int ans = 0, n = nums.size();
-        for (int i = 0; i < n; i++)
-            for (int j = i + 1; j < n; j++) {
-                int product = nums[i] * nums[j];
-                ans += 8 * mp[product];
-                mp[product]++;
+        int result = 0;
+        int n = nums.size();
+        vector<int> productPairs;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++)
+                productPairs.push_back(nums[i] * nums[j]);
+        }
+
+        sort(productPairs.begin(), productPairs.end());
+
+        int lastProduct = -1;
+        int sameProductCount = 0;
+
+        for (int i = 0; i < productPairs.size(); i++) {
+            if (productPairs[i] == lastProduct)
+                sameProductCount++;
+            else {
+                int equalProductPairs = sameProductCount * (sameProductCount - 1) / 2;
+                result += 8 * equalProductPairs;
+                lastProduct = productPairs[i];
+                sameProductCount = 1;
             }
-        return ans;
+        }
+        int equalProductPairs = sameProductCount * (sameProductCount - 1) / 2;
+        result += 8 * equalProductPairs;
+        return result;
     }
 };
