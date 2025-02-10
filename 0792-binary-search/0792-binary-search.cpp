13fc9a8af564cabb17764b73933a6939c9b3f7ed
Ravneet Singh <ravneetsingh290@gmail.com>
2025-02-10 17:55:06 +0530

    Time: 0 ms (100%), Space: 31.2 MB (79.92%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int k) {
+        int s = 0, e = nums.size() - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] == k)
+                return mid;
+            else if (nums[mid] < k)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+
+        return -1;
+    }
+};
