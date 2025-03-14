1488c17cdb342e583e9f254dae68a9e271cc67b8
Ravneet Singh <ravneetsingh290@gmail.com>
2025-03-14 15:31:09 +0530

    Time: 114 ms (5.02%), Space: 88.1 MB (73.33%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,25 +1,21 @@
 class Solution {
-public:
-    int maximumCandies(vector<int>& candies, long long k) {
-        long long left = 1, right = *max_element(candies.begin(), candies.end());
-        int result = 0;
-
-        while (left <= right) {
-            long long mid = left + (right - left) / 2;
-            long long children_count = 0;
-
-            for (int pile : candies) {
-                children_count += pile / mid;
-            }
-
-            if (children_count >= k) {
-                result = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    bool isPossible(vector<int>& can, long long mid, long long k) {
+        long long total = 0;
+        for(auto x: can) {
+            total += x / mid;
         }
-
-        return result;
+        return total >= k;
+    }
+public:
+    int maximumCandies(vector<int>& can, long long k) {
+        long long n = can.size(), end = 0, start = 1;
+        for(auto x: can) end += x;
+        end /= k;
+        while(start <= end) {
+            long long mid = start + (end - start) / 2;
+            if(isPossible(can, mid, k)) start = mid + 1;
+            else end = mid - 1;
+        }
+        return end;
     }
 };
