428703eee4981d616857c5e86e798b5a1360601b
Ravneet Singh <ravneetsingh290@gmail.com>
2025-09-22 17:47:29 +0530

    Time: 0 ms (100%), Space: 10.9 MB (41.26%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -12,23 +12,46 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> pred;
-        if(root == NULL) {
-            return pred;
-        }
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()){
-            root = st.top();
-            st.pop();
-            pred.push_back(root->val);
-            if(root -> right != NULL){
-                st.push(root->right);
+        // vector<int> pred;
+        // if(root == NULL) {
+        //     return pred;
+        // }
+        // stack<TreeNode*> st;
+        // st.push(root);
+        // while(!st.empty()){
+        //     root = st.top();
+        //     st.pop();
+        //     pred.push_back(root->val);
+        //     if(root -> right != NULL){
+        //         st.push(root->right);
+        //     }
+        //     if(root -> left != NULL){
+        //         st.push(root->left);
+        //     }
+        // }
+        // return pred;
+        // Morris ->
+        vector<int> ans;
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* IP = curr->left;
+                while (IP->right != NULL && IP->right != curr) {
+                    IP = IP->right;
+                }
+                if (IP->right == NULL) {
+                    ans.push_back(curr->val);
+                    IP->right = curr;
+                    curr = curr->left;
+                } else {
+                    IP->right = NULL;
+                    curr = curr->right;
+                }
             }
-            if(root -> left != NULL){
-                st.push(root->left);
-            }
         }
-        return pred;
+        return ans;
     }
 };
